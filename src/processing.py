fd26bd0604ce84403eb23769b692a0a5937f68c9
KenDaRat <vedernikovaea.hse@gmail.com>
2026-02-01 17:22:00 +0700

    update processing

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,14 @@
-def filter_by_state(data, state='EXECUTED'):
+def filter_by_state(data, state='EXECUTED') -> list[str]:
+    """
+    Функция принимает список словарей  и возвращает те,
+    у которых ключ соответвует указанному значению
+    """
     return [item for item in data if item.get('state') == state]
 
 
-def sort_by_date(data, reverse=True):
+def sort_by_date(data, reverse=True) -> list[str]:
+    """
+    Функция принимает список словарей и параметр,
+    задающий порядок сортировки и возвращает новый список, откортированный по дате
+    """
     return sorted(data, key=lambda x: x.get('date', ''), reverse=reverse)
